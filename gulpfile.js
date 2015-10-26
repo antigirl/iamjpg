d105761418d77f48c69829a708453bc37cc1e056
antigirl <qasimf@tcd.ie>
2015-10-26 20:40:17 +0000

    removed need to calculate img widths

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,6 @@ var browserify = require('browserify');
 var gm = require('gulp-gm');
 var imagemin = require('gulp-imagemin');
 var clean = require('gulp-clean');
-var jsdom = require('jsdom');
-var sizeOf = require('image-size');
-var fs = require('fs');
-var path = require('path');
-
 var Metalsmith = require('metalsmith');
 var layouts = require('metalsmith-layouts');
 
@@ -53,43 +48,6 @@ gulp.task('metalsmith', ['cleanTemplates'], function() {
 gulp.task('metalsmith:watch', function () {
   gulp.watch(['./src/*.html','./layouts/*.html'], ['metalsmith'])
 });
-
-gulp.task('updateImageWidths:watch', function () {
-  gulp.watch(['./src/*.html'])
-  .on('change', function(file) {
-      var theFile = file.path;
-      updateImageWidths(theFile);
-  });
-});
-
-gulp.task('updateImageWidths', function () {
-    var src = __dirname+'/src';
-    fs.readdir(src, function(err, files) {
-        files.map(function (file) {
-            updateImageWidths(path.join(src, file));
-        });
-    });
-});
-
-function updateImageWidths(theFile) {
-    var totalWidth = 0;
-    var imageMarginRight = 3;
-    var marginLeftFirst = 270;
-    jsdom.env(
-      theFile,
-      function (err, window) {
-          var imagesArray = Array.prototype.slice.call(window.document.getElementsByClassName('iamjpg'), '');
-          imagesArray.forEach(function (el, i) {
-              var imagePath = el.getAttribute('src').replace('dist/', '').replace('lqt/', '');
-              totalWidth += sizeOf(imagePath).width + imageMarginRight;
-          });
-          var data = fs.readFileSync(theFile, 'utf-8');
-          var newValue = data.replace(/containerWidth: .*/, 'containerWidth: ' + parseInt(totalWidth + marginLeftFirst));
-          fs.writeFileSync(theFile, newValue, 'utf-8');
-          console.log('imageWidth updated for template ' + theFile);
-      }
-    );
-}
 
 gulp.task('sass', function () {
   gulp.src('./styles/styles.scss')
