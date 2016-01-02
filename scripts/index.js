ef8d6c6969bcb026031ac5880b165cb73439bae5
Farzad Qasim <qasimf@tcd.ie>
2016-01-02 18:07:42 +0000

    GA tracking and resize update

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,10 @@ var lazyload = require('./lazyload');
 var actions = require('./actions');
 lazyload.init();
 actions.init();
+
+var photoWrapper = document.getElementsByClassName('photo-wrapper')[0];
+
+window.addEventListener('resize', function () {
+    console.log('resizing,', window.innerHeight);
+    photoWrapper.style.height = window.innerHeight + 'px';
+});
